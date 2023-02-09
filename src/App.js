edf63ca09f571a2c8022c7ad62eff98232b2c752
Heidi Kuhnau <95493237+hkuhnau@users.noreply.github.com>
2023-02-09 10:55:13 -0600

    updated the front end app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// Import useState and useEffect hooks from React
+import React, { useState, useEffect } from 'react'
+
+// Import the API category from AWS Amplify
+import { API } from 'aws-amplify'
+
 import './App.css';
 
 function App() {
+  // Create coins variable and set to empty array
+  const [items, updateCoins] = useState([])
+
+  // Define function to all API
+  async function fetchCoins() {
+    const data = await API.get('project3', '/items')
+    updateCoins(data.items)
+  }
+
+  // Call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        items.map((item, index) => (
+          <div key={index}>
+            <h2>{item.name} - {item.symbol}</h2>
+            <h5>${item.price_usd}</h5>
+          </div>
+        ))
+      }
     </div>
   );
 }
 
-export default App;
+export default App
